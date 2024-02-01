fdd30994eb8687b0ac78a3aeab44f528c56ec32d
kyouhei14 <158478884+kyouhei14@users.noreply.github.com>
2024-02-01 14:39:09 +0000

    Initial commit

---
+++ scripts/cmds/info.js
@@ -0,0 +1,43 @@
+const { getStreamFromURL } = require("fb-watchman");
+module.exports = {
+  config: {
+    name: "info",
+    version: 2.0,
+    author: "OtinXSandip",
+    longDescription: "info about bot and owner",
+    category: "ai",
+    guide: {
+      en: "{p}{n}",
+    },
+  },
+
+  onStart: async function ({ api, event, args, message, usersData }) {
+    const imgURL = "https://i.imgur.com/MEYPUFF.jpg";
+    const attachment = await global.utils.getStreamFromURL(imgURL);
+
+    const id = event.senderID;
+    const userData = await usersData.get(id);
+    const name = userData.name;
+
+    const ment = [{ id: id, tag: name }];
+    const a = "Gekko 🐉";
+    const b = " + ";
+    const c = "Rajveer👾";
+const e = "Male";
+    const d = "m.me/kyouhei8";
+const f = "yng.kyouhei";
+const g = "Norzum🤍";
+
+    message.reply({ 
+      body: `${name}, here is the information 🌝
+🌸 Bot's Name: ${a}
+🌸 Bot's prefix: ${b}  
+🌸 Owner: ${c}
+🌸 Gender: ${e}
+🌸 Messenger: ${d}
+🌸 Insta: ${f}
+🌸 Relationship: ${g}`,
+mentions: ment,
+      attachment: attachment });
+  }
+};
